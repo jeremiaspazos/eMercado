3a8293d718e049de5980ca5e8a5c4621e225870c
jeremiaspazos <jeremias.pazos@hotmail.com>
2020-08-05 12:39:45 -0300

    Products Funcional

--- js/products.js
+++ js/products.js
@@ -1,8 +1,45 @@
+//Funcion para introducir el JSON a nuestro HTML
 var productsArray = []
+
+function showProductsList(array){
+
+    let htmlContentToAppend = "";
+    for(let i = 0; i < array.length; i++){
+        let products= array[i];
+
+        htmlContentToAppend += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + products.imgSrc + `" alt="` + products.desc + `" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        
+                        <h4 class="mb-1">`+ products.name +`</h4>
+                        <small class="text-muted">` + products.soldCount + ` artículos</small>
+                    </div>
+                    <div>` + products.description + `</div>
+
+                </div>
+            </div>
+        </div>
+        `
+
+        document.getElementById("container-products").innerHTML = htmlContentToAppend;//nombre de ID debe corregirse.
+    }
+}
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
-    console.log(fetch(PRODUCTS_URL))  
-
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(PRODUCTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            productsArray = resultObj.data;
+            //Muestro las categorías ordenadas
+            showProductsList(productsArray);
+        }
+    });
 });
