2e2dad746630d514b1fc0498c96a26e81e110576
jeremiaspazos <jeremias.pazos@hotmail.com>
2020-08-26 14:25:56 -0300

    NavBar Dinamica

--- js/init.js
+++ js/init.js
@@ -48,7 +48,13 @@ if (!sessionStorage.getItem('logueado') &&
 //funcion para incrustar nombre de usuario en NavBar
 function escribirUsuario(){
   var usuario2 = JSON.parse(localStorage.getItem("usuarioAndPass"));
-  document.getElementById("profileLink").innerHTML=usuario2.nombre;
+  
+  var usuarioNav = document.createElement("a")
+  usuarioNav.setAttribute("id", "nombreUsuario")
+  usuarioNav.setAttribute("class","py-2 d-none d-md-inline-block");
+  usuarioNav.setAttribute("href", "my-profile.html");
+  usuarioNav.appendChild(document.createTextNode(usuario2.nombre));
+  document.querySelectorAll('nav.site-header div')[0].appendChild(usuarioNav)
   }
   escribirUsuario();
 
