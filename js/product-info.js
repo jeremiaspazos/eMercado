b7868eee90ed499b8a42c3640b25257bec289737
jeremiaspazos <jeremias.pazos@hotmail.com>
2020-09-04 13:48:15 -0300

    Product-Info + Comment

--- js/product-info.js
+++ js/product-info.js
@@ -18,6 +18,25 @@ function showImagesGallery(array){
 }
 
 
+function showComments(array){
+
+    let htmlContentToAppend = "";
+
+    for(let i = 0; i < array.length; i++){
+    
+    let comment = array[i]
+
+    htmlContentToAppend += `<div id="coments" class="card text-left">
+    <h3>`+ comment.user + `</h3>
+    <p class ="font-italic">`+comment.description +`</p>
+    <p>`+comment.score +`</p>
+    <small>`+comment.dateTime +`</small>
+    
+  </div>`
+    }
+    document.getElementById("comment-container").innerHTML = htmlContentToAppend
+}
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
@@ -40,3 +59,14 @@ document.addEventListener("DOMContentLoaded", function(e){
         }
     });
 });
+
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok"){
+            commentsArray = resultObj.data;
+
+
+        showComments(commentsArray);
+        }
+    })
+})
