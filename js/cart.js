4096b7df48a3e26c845eef3585fa9ee98eb4d9c6
Jeremias <jeremias.pazos@hotmail.com>
2020-11-08 22:39:36 -0300

    Entrega 7

--- js/cart.js
+++ js/cart.js
@@ -1,29 +1,38 @@
 var htmlContentToAppend = "";
-var cartArray=[];
+var cartArray = [];
 var USD = 40;
-let pesos = 0
-let pesos2 = 0  
+let pesos = 0;
+let pesos2 = 0;
 var tipoEnvio = 1.15;
 
 
 
-function totals  () {
-    let subtotalFinal = document.getElementById("subtotalN").innerHTML
+function totals() {
+    let subtotalFinal = document.getElementById("subtotalN").innerHTML;
 
-     var total = subtotalFinal * tipoEnvio
-     var costoEnvio = total - subtotalFinal
-     document.getElementById("costoEnvio").innerHTML =`Envio: `+costoEnvio.toFixed(2)+``
-     document.getElementById("total").innerHTML =`Total: `+total.toFixed(2)+``
-    
-    
-}
+    var total = subtotalFinal * tipoEnvio;
+    var costoEnvio = total - subtotalFinal;
+    let dolarCheck = document.getElementById("dolar").checked;
+    let pesosCheck = document.getElementById("pesos").checked;
+    let dolar = document.getElementById("dolar").value;
+    let pesos = document.getElementById("pesos").value;
+    if (dolarCheck === true) {
 
-function cartDisplay (array){
-    
-    for(i = 0 ; i < array.length; i++){
-      let  article = array[i]
-      
-     htmlContentToAppend += `
+        document.getElementById("costoEnvio").innerHTML = `Envio: ` + costoEnvio.toFixed(2) + `` + dolar + ` `;
+        document.getElementById("total").innerHTML = `Total: ` + total.toFixed(2) + `` + dolar + ``;
+    } else {
+        document.getElementById("costoEnvio").innerHTML = `Envio: ` + costoEnvio.toFixed(2) + `` + pesos + ` `;
+        document.getElementById("total").innerHTML = `Total: ` + total.toFixed(2) + `` + pesos + ` `;
+    }
+
+};
+
+function cartDisplay(array) {
+
+    for (i = 0; i < array.length; i++) {
+        let article = array[i];
+
+        htmlContentToAppend += `
         <div class="list-group-item list-group-item-action" id="`+ article.name + `" style="border-radius: 15px; margin-top: 5px;">
             <div class="row">
                 <div class="col-3">
@@ -32,11 +41,11 @@ function cartDisplay (array){
                 <div class="col">
                     <div class="d-flex w-100 justify-content-between">
                         <h4 class="mb-1">`+ article.name + `</h4>
-                        <p class="text-muted">` + article.unitCost + ` `+article.currency+`  </p>
+                        <p class="text-muted">` + article.unitCost + ` ` + article.currency + `  </p>
                     </div>
                 <div class="col-3">
-                <label for="count`+i+`">Cantidad<input type="number" min="1" id="count`+i+`" value="`+article.count+`" name="count`+i+`" onkeyup="articleSubtotals(cartArray)" onchange="articleSubtotals(cartArray)"></label>
-                <p id="subtotal`+i+`">Subtotal por articulo = </p>
+                <label for="count`+ i + `">Cantidad<input type="number" min="1" id="count` + i + `" value="` + article.count + `" name="count` + i + `" onkeyup="articleSubtotals(cartArray)" onchange="articleSubtotals(cartArray)"></label>
+                <p id="subtotal`+ i + `">Subtotal por articulo = </p>
                 </div>
                 </div>
             </div>
@@ -44,111 +53,110 @@ function cartDisplay (array){
         `
 
     }
-    document.getElementById("container-cart").innerHTML = htmlContentToAppend
-}
+    document.getElementById("container-cart").innerHTML = htmlContentToAppend;
+};
 
 
 
-function articleSubtotals(array){
-    for(i=0 ; i<array.length ; i++){
+function articleSubtotals(array) {
+    for (i = 0; i < array.length; i++) {
         let article = array[i];
-        let count="count";
-        let sub="subtotal";
-        let subtotals= document.getElementById(sub+i);
-        let inputs = document.getElementById(count+i).value;
+        let count = "count";
+        let sub = "subtotal";
+        let subtotals = document.getElementById(sub + i);
+        let inputs = document.getElementById(count + i).value;
         let suma = article.unitCost * inputs;
-        document.getElementById(sub+i).innerHTML = `El subtotal por articulo es de `+suma+` `+article.currency+``
-        
-     
-    }
-    
-}; 
+        document.getElementById(sub + i).innerHTML = `El subtotal por articulo es de ` + suma + ` ` + article.currency + ``;
 
-function subtotalUyu(array){
-    for(i=0; i<array.length ; i++) {
-        let article = array[i];
-        let sub="subtotal";
-        let currency = array[i].currency
-        
-        let count="count";
-        let inputs = document.getElementById(count+i).value;
-        let suma = article.unitCost * inputs;
-        if(currency != "UYU"){
-            pesos = suma * USD
-        } else {
-            pesos2 = suma
-        }
-        suma = pesos + pesos2
-        document.getElementById("subtotalCompra").innerHTML = `<p> El subtotal es de `+array[0].currency+` <p id="subtotalN" style="font-size:20px;">`+suma+`</p></p>`
-        totals()
-    }
 
-    
+    }
 
 };
-function subtotalUSD(array){
-    for(i=0; i<array.length ; i++) {
+
+function subtotalUyu(array) {
+    for (i = 0; i < array.length; i++) {
         let article = array[i];
-        let sub="subtotal";
-        let currency = array[i].currency
-        
-        let count="count";
-        let inputs = document.getElementById(count+i).value;
+        let sub = "subtotal";
+        let currency = array[i].currency;
+        let count = "count";
+        let inputs = document.getElementById(count + i).value;
         let suma = article.unitCost * inputs;
-        if(currency == "UYU"){
+
+        if (currency != "UYU") {
+            pesos = suma * USD;
+        } else {
+            pesos2 = suma;
+        }
+        suma = pesos + pesos2;
+        document.getElementById("subtotalCompra").innerHTML = `<p> El subtotal es de ` + array[0].currency + ` <p id="subtotalN" style="font-size:20px;">` + suma + `</p></p>`
+        totals();
+    }
+
+
+
+};
+function subtotalUSD(array) {
+    for (i = 0; i < array.length; i++) {
+        let article = array[i];
+        let sub = "subtotal";
+        let currency = array[i].currency
+        let count = "count";
+        let inputs = document.getElementById(count + i).value;
+        let suma = article.unitCost * inputs;
+        
+        if (currency == "UYU") {
             pesos = suma / USD;
         } else {
             pesos2 = suma;
         }
         suma = pesos + pesos2;
-        document.getElementById("subtotalCompra").innerHTML = `<p> El subtotal es de `+array[1].currency+` <p id="subtotalN" style="font-size:20px;">`+suma+`</p></p>`;
-        totals()
+        document.getElementById("subtotalCompra").innerHTML = `<p> El subtotal es de ` + array[1].currency + ` <p id="subtotalN" style="font-size:20px;">` + suma + `</p></p>`;
+        totals();
     }
-
-    
-
 };
-function exchanger(array){
-    let dolar = document.getElementById("dolar").checked
-    let pesos = document.getElementById("pesos").checked
-    let input0= document.getElementById("count0")
-    let input1= document.getElementById("count1")
-    
-    if( dolar == true ){
-        input0.setAttribute("onchange", "subtotalUSD(cartArray), articleSubtotals(cartArray)")
-        input0.setAttribute("onkeyup", "subtotalUSD(cartArray), articleSubtotals(cartArray)")
-        input1.setAttribute("onchange", "subtotalUSD(cartArray), articleSubtotals(cartArray)")
-        input1.setAttribute("onkeyup", "subtotalUSD(cartArray), articleSubtotals(cartArray)")
-    } else {
-        input0.setAttribute("onchange", "subtotalUyu(cartArray), articleSubtotals(cartArray)")
-        input0.setAttribute("onkeyup", "subtotalUyu(cartArray), articleSubtotals(cartArray)")
-        input1.setAttribute("onchange", "subtotalUyu(cartArray), articleSubtotals(cartArray)")
-        input1.setAttribute("onkeyup", "subtotalUyu(cartArray), articleSubtotals(cartArray)")
-        
-    }
-}
 
-function validatePayDebit(e){
+
+function exchanger(array) {
+    let dolar = document.getElementById("dolar").checked;
+    let pesos = document.getElementById("pesos").checked;
+    let input0 = document.getElementById("count0");
+    let input1 = document.getElementById("count1");
+
+    if (dolar == true) {
+        input0.setAttribute("onchange", "subtotalUSD(cartArray), articleSubtotals(cartArray)");
+        input0.setAttribute("onkeyup", "subtotalUSD(cartArray), articleSubtotals(cartArray)");
+        input1.setAttribute("onchange", "subtotalUSD(cartArray), articleSubtotals(cartArray)");
+        input1.setAttribute("onkeyup", "subtotalUSD(cartArray), articleSubtotals(cartArray)");
+    } else {
+        input0.setAttribute("onchange", "subtotalUyu(cartArray), articleSubtotals(cartArray)");
+        input0.setAttribute("onkeyup", "subtotalUyu(cartArray), articleSubtotals(cartArray)");
+        input1.setAttribute("onchange", "subtotalUyu(cartArray), articleSubtotals(cartArray)");
+        input1.setAttribute("onkeyup", "subtotalUyu(cartArray), articleSubtotals(cartArray)");
+
+    }
+};
+
+function validatePayDebit(e) {
     let debitInput = document.getElementById("tarjetaDebito").value;
     let debitExpress = /^\d{16}$/
-    
-       
-    if(!debitExpress.test(debitInput)){
+
+
+    if (!debitExpress.test(debitInput)) {
         e.preventDefault();
         alert("Informacion Invalida. Ingresar 16 digitos, sin incluir guion o espacios.");
-       
+
     }
-    else{
-        alert('Datos enviados con exito.')
-        event.preventDefault()
+    else {
+        alert('Datos enviados con exito.');
+        event.preventDefault();
         let debitNumSt = JSON.stringify(debitInput);
-        localStorage.setItem("numero", debitNumSt);
+        sessionStorage.setItem("numero", debitNumSt);
         $('#debitModal').modal('hide');
     }
-    
+
 }
 
-function validatePay(e){
+function validatePay(e) {
     let creditInput = document.getElementById("tarjetaCredito").value;
     let creditExpress = /^\d{16}$/
     let cvvInput = document.getElementById("identificador").value;
@@ -158,84 +166,95 @@ function validatePay(e){
     let yearInput = document.getElementById("año").value;
     let yearExpress = /^\d{4}$/
 
-    if (!(monthExpress.test(monthInput))){
+    if (!(monthExpress.test(monthInput))) {
         e.preventDefault();
         alert("Informacion Invalida. Debe ser un mes entre 01 y 12.");
-       
-    }else
-   
-    if(!creditExpress.test(creditInput)){
-        e.preventDefault();
-        alert("Informacion Invalida. Ingresar 16 digitos, sin incluir guion o espacios.");
-       
-    }else
-    
-    if(!cvvExpress.test(cvvInput)){
-        e.preventDefault();
-        alert("Informacion Invalida. Ingresar un numero de 3 digitos");
-       
-    }else
-   
-    
-    if(!yearExpress.test(yearInput)){
-        e.preventDefault();
-        alert("Informacion Invalida. Ingresar un numero de 4 digitos");
-        
-    }
-    else{
-        alert('Datos enviados con exito.')
-        event.preventDefault()
-        
-        let creditNumSt = JSON.stringify(creditInput);
-        sessionStorage.setItem("numero", creditNumSt);
-        let cvvNumSt = JSON.stringify(cvvInput);
-        sessionStorage.setItem("verificador", cvvNumSt);
-        let monthNumSt = JSON.stringify(monthInput);
-        sessionStorage.setItem("mes", monthNumSt);
-        let yearNumSt = JSON.stringify(yearInput);
-        sessionStorage.setItem("año", yearNumSt);
-        $('#cardModal').modal('hide');
-    }
-    
-}
 
-document.addEventListener("DOMContentLoaded", function(e){
-fetch(CART_INFO_URL).then(data => data.json())
-.then((data) =>{
-    cartArray = data.articles
-    
-    cartDisplay(cartArray);
-    articleSubtotals(cartArray);
-    exchanger(cartArray);
-    subtotalUSD(cartArray)
-   
-    });
-    document.getElementById("dolar").addEventListener("click", function(){
+    } else
+
+        if (!creditExpress.test(creditInput)) {
+            e.preventDefault();
+            alert("Informacion Invalida. Ingresar 16 digitos, sin incluir guion o espacios.");
+
+        } else
+
+            if (!cvvExpress.test(cvvInput)) {
+                e.preventDefault();
+                alert("Informacion Invalida. Ingresar un numero de 3 digitos");
+
+            } else
+
+
+                if (!yearExpress.test(yearInput)) {
+                    e.preventDefault();
+                    alert("Informacion Invalida. Ingresar un numero de 4 digitos");
+
+                }
+                else {
+                    alert('Datos enviados con exito.')
+                    event.preventDefault()
+
+                    let creditNumSt = JSON.stringify(creditInput);
+                    sessionStorage.setItem("numero", creditNumSt);
+                    let cvvNumSt = JSON.stringify(cvvInput);
+                    sessionStorage.setItem("verificador", cvvNumSt);
+                    let monthNumSt = JSON.stringify(monthInput);
+                    sessionStorage.setItem("mes", monthNumSt);
+                    let yearNumSt = JSON.stringify(yearInput);
+                    sessionStorage.setItem("año", yearNumSt);
+                    $('#cardModal').modal('hide');
+                }
+
+}
+function validacionMaxima(e){
+    var numero = JSON.parse(sessionStorage.getItem("numero"));
+
+    if (numero === null){
+        e.preventDefault()
+        alert('Ingrese datos en tipo de pago!')
+    }
+};
+
+document.addEventListener("DOMContentLoaded", function (e) {
+    fetch(CART_INFO_URL).then(data => data.json())
+        .then((data) => {
+            cartArray = data.articles
+
+            cartDisplay(cartArray);
+            articleSubtotals(cartArray);
+            exchanger(cartArray);
+            subtotalUSD(cartArray)
+
+        });
+    document.getElementById("dolar").addEventListener("click", function () {
         exchanger(cartArray)
         subtotalUSD(cartArray)
     });
-    document.getElementById("pesos").addEventListener("click", function(){
+    document.getElementById("pesos").addEventListener("click", function () {
         exchanger(cartArray)
         subtotalUyu(cartArray)
     });
-    document.getElementById("premiumradio").addEventListener("change", function(){
+    document.getElementById("premiumradio").addEventListener("change", function () {
         tipoEnvio = 1.15;
         totals();
     });
-    
-    document.getElementById("expressradio").addEventListener("change", function(){
+
+    document.getElementById("expressradio").addEventListener("change", function () {
         tipoEnvio = 1.07;
         totals();
     });
 
-    document.getElementById("standardradio").addEventListener("change", function(){
+    document.getElementById("standardradio").addEventListener("change", function () {
         tipoEnvio = 1.05;
         totals();
     });
-    document.getElementById("submit-credit").addEventListener("click", function(){
+    document.getElementById("submit-credit").addEventListener("click", function () {
         validatePay(event);
     });
-    document.getElementById("submit-debit").addEventListener("click", function(){
+    document.getElementById("submit-debit").addEventListener("click", function () {
         validatePayDebit(event);
+    });
+    document.getElementById("comprar-button").addEventListener("click", function () {
+        validacionMaxima(event);
     });
 });
--- js/my-profile.js
+++ js/my-profile.js
@@ -1,6 +1,35 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
+let nombre =  document.getElementById("nombre")
+let edad =  document.getElementById("edad")
+let tel =  document.getElementById("tel")
+let email =  document.getElementById("email")
 
+function modificarDatos() {
+   nombre.disabled = false;
+   edad.disabled = false;
+   tel.disabled = false;
+   email.disabled = false;
+} 
+function guardarDatos() {
+    nombre.disabled = true;
+   edad.disabled = true;
+   tel.disabled = true;
+   email.disabled = true;
+   nombreString = JSON.stringify(nombre.value)
+   edadString = JSON.stringify(edad.value)
+   telString = JSON.stringify(tel.value)
+   emailString = JSON.stringify(email.value)
+   localStorage.setItem("nombre", nombreString)
+   localStorage.setItem("edad", edadString)
+   localStorage.setItem("tel", telString)
+   localStorage.setItem("email", emailString)
+}
+
+
+document.addEventListener("DOMContentLoaded", function (e) {
+      if(localStorage.getItem("edad") != null){
+         nombre.value = JSON.parse(localStorage.getItem("nombre"))
+         edad.value = JSON.parse(localStorage.getItem("edad"))
+         email.value = JSON.parse(localStorage.getItem("email"))
+         tel.value = JSON.parse(localStorage.getItem("tel"))
+      }
 });
